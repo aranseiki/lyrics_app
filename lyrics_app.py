78cf1bdc71737c9ebffa19bfa2458839980a3247
Allan de Oliveira Almeida <techall@hotmail.com.br>
2023-05-24 22:50:52 -0300

    POC

---
+++ lyrics_app.py
@@ -0,0 +1,121 @@
+from pprint import pprint
+from requests import get
+from json import loads
+from py_rpautom.python_utils import ler_variavel_ambiente
+
+def cls():
+    import os
+    os.system('cls')
+
+cls()
+token_lyrics_app = ler_variavel_ambiente(
+    nome_variavel='token_lyrics_app',
+    variavel_sistema=True,
+)
+header = {
+    'Authorization': f'Bearer {token_lyrics_app}'
+}
+url_base = 'https://api.genius.com/'
+endpoint = 'search?q=colbie%20caillat'
+
+data1 = loads(get(url=''.join((url_base, endpoint)), headers=header).content)
+
+artist_id = ''
+for response in data1['response']['hits']:
+    try:
+        infobox1 = {
+            'artist_name' : response['result']['primary_artist']['name'],
+            'artist_id' : response['result']['primary_artist']['id'],
+            'full_title' : response['result']['full_title']
+        }
+        if infobox1['artist_name'].upper() == 'COLBIE CAILLAT':
+            artist_id = infobox1['artist_id']
+            break
+    except:
+        ...
+
+endpoint = f'artists/{artist_id}'
+data2 = loads(get(url=''.join((url_base, endpoint, '/songs')), headers=header).content)
+
+title_song = ''
+for response in data2['response']['songs']:
+    try:
+        infobox2 = {
+            'title' : response['title'],
+            'url': ''
+        }
+
+        if infobox2['title'].upper() == 'BEFORE I LET YOU GO':
+            title_song = infobox2['title']
+            infobox2['url'] = response['url']
+            break
+
+    except:
+        ...
+
+html_content = get(infobox2['url'])
+
+validation_show = False
+validation_number = 0
+skiping_list = [
+    b'",{',
+    b'"tag',
+    b'":',
+    b'"br',
+    b'"},',
+    b'/a><br>',
+    b'',
+    b'"html',
+    b'"body',
+    b'"data',
+    b'":{',
+    b'"name',
+    b'"inread-ad',
+    b'"p',
+    b'"',
+    b"'",
+    b"desktop_",
+    b'"root',
+    b'"],',
+    b'"},{',
+    b'"a',
+    b'"href',
+    b'"attributes',
+    b'"id',
+    b'"pending',
+    b'"editorialState',
+    b'"unreviewed',
+    b'",',
+    b'"classification',
+    b'":[',
+    b'":[{',
+    b'"children',
+    b'" data-id=',
+    b'"17777149',
+    b'n<a href=',
+    b'n<a href=',
+]
+
+infobox3 = {
+    'artist_name' : infobox1['artist_name'],
+    'title' : infobox2['title']
+}
+
+lyrics_content = []
+for item in b''.join(html_content.content.splitlines()).split(b'\\'):
+    if item.__contains__(b'"body') and validation_number < 1:
+        validation_number = validation_number + 1
+        validation_show = True
+
+    if item.__contains__(b'lyricsPlaceholderReason'):
+        validation_show = False
+
+    if validation_show is True and item not in skiping_list:
+        lyrics_content.append(item)
+        ...
+
+print(infobox3['artist_name'])
+print(infobox3['title'], '\n')
+[print(verse.decode(encoding='utf8')) for verse in lyrics_content]
+
+# pprint(loads(get(url=''.join((url_base, endpoint)), headers=header).content))
