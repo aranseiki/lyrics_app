97879ff572c7a984a0fc5de49f820747269384d3
Allan de Oliveira Almeida <techall@hotmail.com.br>
2023-05-25 00:08:45 -0300

    modularização da musica e artista

--- lyrics_app.py
+++ lyrics_app.py
@@ -11,138 +11,138 @@ token_lyrics_app = ler_variavel_ambiente(
 )
 header = {'Authorization': f'Bearer {token_lyrics_app}'}
 
-artist = 'Vanessa Carlton'
-music = 'Pretty Baby'
+def search_lyrics(artist, music,):
 
-url_base = 'https://api.genius.com/'
-endpoint = f'search?q={artist.upper()}'
+    url_base = 'https://api.genius.com/'
+    endpoint = f'search?q={artist.upper()}'
 
-data1 = loads(get(url=''.join((url_base, endpoint)), headers=header).content)
-
-artist_id = ''
-for response in data1['response']['hits']:
-    try:
-        infobox1 = {
-            'artist_name': response['result']['primary_artist']['name'],
-            'artist_id': response['result']['primary_artist']['id'],
-            'full_title': response['result']['full_title'],
-        }
-
-        if infobox1['artist_name'].upper() == artist.upper():
-            artist_id = infobox1['artist_id']
-            break
-    except:
-        ...
-
-page_number = 1
-# artists/8568/songs?page=30
-next_page = None
-validation_title = False
-title_song = ''
-while not next_page == 'null':
-    print('next_page:', page_number)
-    endpoint = f'artists/{artist_id}/songs?page={page_number}'
-    data2 = loads(
-        get(
-            url=''.join((url_base, endpoint, '/songs')), headers=header
-        ).content
+    data1 = loads(
+        get(url=''.join((url_base, endpoint)), headers=header).content
     )
-    next_page = data2['response']['next_page']
 
-    if next_page is None:
-        next_page = 'null'
-        continue
-
-    page_number = next_page
-
-    for response in data2['response']['songs']:
+    artist_id = ''
+    for response in data1['response']['hits']:
         try:
-            infobox2 = {
-                'title': response['title'],
-                'url': ''
+            infobox1 = {
+                'artist_name': response['result']['primary_artist']['name'],
+                'artist_id': response['result']['primary_artist']['id'],
+                'full_title': response['result']['full_title'],
             }
 
-            if (
-                remover_acentos(infobox2['title'].upper())
-                == remover_acentos(music).upper()
-            ):
-                title_song = infobox2['title']
-                infobox2['url'] = response['url']
-                validation_title = True
+            if infobox1['artist_name'].upper() == artist.upper():
+                artist_id = infobox1['artist_id']
                 break
-
         except:
             ...
-    if validation_title is True:
-        break
 
-if (next_page == 'null') and (title_song == ''):
-    raise SystemError(
-        'Não foi possível localizar a música solicitada.'
-    )
+    page_number = 1
+    next_page = None
+    validation_title = False
+    title_song = ''
+    while not next_page == 'null':
+        print('next_page:', page_number)
+        endpoint = f'artists/{artist_id}/songs?page={page_number}'
+        data2 = loads(
+            get(
+                url=''.join((url_base, endpoint, '/songs')), headers=header
+            ).content
+        )
+        next_page = data2['response']['next_page']
 
-html_content = get(infobox2['url'])
+        if next_page is None:
+            next_page = 'null'
+            continue
 
-validation_show = False
-validation_number = 0
-skiping_list = [
-    b'",{',
-    b'"tag',
-    b'":',
-    b'"br',
-    b'"},',
-    b'/a><br>',
-    b'',
-    b'"html',
-    b'"body',
-    b'"data',
-    b'":{',
-    b'"name',
-    b'"inread-ad',
-    b'"p',
-    b'"',
-    b"'",
-    b'desktop_',
-    b'"root',
-    b'"],',
-    b'"},{',
-    b'"a',
-    b'"href',
-    b'"attributes',
-    b'"id',
-    b'"pending',
-    b'"editorialState',
-    b'"unreviewed',
-    b'",',
-    b'"classification',
-    b'":[',
-    b'":[{',
-    b'"children',
-    b'" data-id=',
-    b'"17777149',
-    b'n<a href=',
-    b'n<a href=',
-]
+        page_number = next_page
 
-infobox3 = {
-    'artist_name': infobox1['artist_name'],
-    'title': infobox2['title']
-}
+        for response in data2['response']['songs']:
+            try:
+                infobox2 = {
+                    'title': response['title'],
+                    'url': ''
+                }
 
-lyrics_content = []
-for item in b''.join(html_content.content.splitlines()).split(b'\\'):
-    if item.__contains__(b'"body') and validation_number < 1:
-        validation_number = validation_number + 1
-        validation_show = True
+                if (
+                    remover_acentos(infobox2['title'].upper())
+                    == remover_acentos(music).upper()
+                ):
+                    title_song = infobox2['title']
+                    infobox2['url'] = response['url']
+                    validation_title = True
+                    break
 
-    if item.__contains__(b'lyricsPlaceholderReason'):
-        validation_show = False
+            except:
+                ...
+        if validation_title is True:
+            break
 
-    if validation_show is True and item not in skiping_list:
-        lyrics_content.append(item)
-        ...
+    if (next_page == 'null') and (title_song == ''):
+        raise SystemError('Não foi possível localizar a música solicitada.')
 
-print(infobox3['artist_name'])
-print(infobox3['title'], '\n')
+    cls()
 
-[print(verse.decode(encoding='utf8')) for verse in lyrics_content]
+    html_content = get(infobox2['url'])
+
+    validation_show = False
+    validation_number = 0
+    skiping_list = [
+        b'",{',
+        b'"tag',
+        b'":',
+        b'"br',
+        b'"},',
+        b'/a><br>',
+        b'',
+        b'"html',
+        b'"body',
+        b'"data',
+        b'":{',
+        b'"name',
+        b'"inread-ad',
+        b'"p',
+        b'"',
+        b"'",
+        b'desktop_',
+        b'"root',
+        b'"],',
+        b'"},{',
+        b'"a',
+        b'"href',
+        b'"attributes',
+        b'"id',
+        b'"pending',
+        b'"editorialState',
+        b'"unreviewed',
+        b'",',
+        b'"classification',
+        b'":[',
+        b'":[{',
+        b'"children',
+        b'" data-id=',
+        b'"17777149',
+        b'n<a href=',
+        b'n<a href=',
+    ]
+
+    infobox3 = {
+        'artist_name': infobox1['artist_name'],
+        'title': infobox2['title']
+    }
+
+    lyrics_content = []
+    for item in b''.join(html_content.content.splitlines()).split(b'\\'):
+        if item.__contains__(b'"body') and validation_number < 1:
+            validation_number = validation_number + 1
+            validation_show = True
+
+        if item.__contains__(b'lyricsPlaceholderReason'):
+            validation_show = False
+
+        if validation_show is True and item not in skiping_list:
+            lyrics_content.append(item)
+            ...
+
+    print(infobox3['artist_name'])
+    print(infobox3['title'], '\n')
+
+    [print(verse.decode(encoding='utf8')) for verse in lyrics_content]
