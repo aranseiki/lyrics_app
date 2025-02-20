bac0b55f39b146e4c668a9c0e861ede701f82f3d
Allan de Oliveira Almeida <arumeiraadan@outlook.com>
2025-02-20 01:54:37 -0300

    Refatoração de código, com DRY e tratamento de erros da API

--- app/generic.py
+++ app/generic.py
@@ -1,97 +1,203 @@
 from json import loads
 from pprint import pprint
 
-from py_rpautom.python_utils import cls, ler_variavel_ambiente, remover_acentos
+from py_rpautom.python_utils import (
+    cls,
+    ler_variavel_ambiente,
+    remover_acentos
+)
 from requests import get
 
-cls()
-token_lyrics_app = ler_variavel_ambiente(
-    nome_variavel='token_lyrics_app',
-    variavel_sistema=True,
-)
-header = {'Authorization': f'Bearer {token_lyrics_app}'}
+
+def api_error_get_song_content(
+        base_data: dict[str, str],
+    ) -> tuple[str, str]:
+        error_description = base_data.get('meta').get('message')
+        error = str(base_data.get('meta').get('status'))
+        return (
+            error,
+            error_description,
+        )
 
 
-def search_lyrics(artist, music,):
+def api_error_validation(
+    base_data: dict[str, str],
+) -> tuple[str, str]:
+    error_description = base_data.get('error_description')
+    error = base_data.get('error')
+    return (
+        error,
+        error_description,
+    )
 
+
+def get_api_auth_bearer(
+    env_var_name: str = 'LYRICS_APP_CLIENT_BEARER',
+) -> dict[str, str]:
+    token_lyrics_app = ler_variavel_ambiente(
+        nome_variavel = env_var_name,
+        variavel_sistema = True,
+    )
+
+    header = {'Authorization': f'Bearer {token_lyrics_app}'}
+
+    return header
+
+
+def get_artist_base_data(
+    artist: str,
+    header: dict[str, str],
+) -> tuple[str, dict[str, str]]:
     url_base = 'https://api.genius.com/'
     search_artist_endpoint = f'search?q={artist.upper()}'
     url_search_artist = ''.join((url_base, search_artist_endpoint))
-    data1 = loads(
+    base_data = loads(
         get(url=url_search_artist, headers=header).content
     )
 
-    artist_id = ''
-    for response in data1['response']['hits']:
-        try:
-            infobox1 = {
-                'artist_name': response['result']['primary_artist']['name'],
-                'artist_id': response['result']['primary_artist']['id'],
-                'full_title': response['result']['full_title'],
-            }
+    error_data = api_error_validation(base_data)
+    if (error_data[0] is not None):
+        raise SystemError(
+            f'Error: {error_data[0]}\n Description: {error_data[1]}'
+        )
+    
+    return url_base, base_data
 
-            if infobox1['artist_name'].upper() == artist.upper():
-                artist_id = infobox1['artist_id']
+
+def get_artist_data_information(
+    artist: str,
+    base_data: dict[str, str],
+) -> dict[str, str]:
+    for song_data in base_data['response']['hits']:
+        selected_data_information = {
+            'artist_name': '',
+            'artist_id': '',
+            'full_title': '',
+        }
+
+        try:
+            if (
+                song_data['result']['primary_artist']['name'].upper()
+                == artist.upper()
+            ):
+                selected_data_information['artist_name'] = song_data[
+                    'result'
+                ]['primary_artist']['name']
+                selected_data_information['artist_id'] = song_data[
+                    'result'
+                ]['primary_artist']['id']
+                selected_data_information['full_title'] = song_data[
+                    'result'
+                ]['full_title']
+
                 break
         except:
             ...
 
-    if artist_id == '':
+    return selected_data_information
+
+
+def get_song_content(
+    artist_id: int,
+    page_number: int,
+    url_base: str,
+    header_api: dict[str, str],
+):
+    print('next_page:', page_number)
+
+    endpoint = f'artists/{artist_id}/songs?page={page_number}'
+    title_song_endpoint = ''.join((url_base, endpoint, '/songs'))
+
+    title_song_content = loads(
+        get(
+            url = title_song_endpoint,
+            headers = header_api,
+        ).content
+    )
+
+    error_data = api_error_get_song_content(title_song_content)
+
+    if (
+        (error_data[0] is not None)
+        and (not error_data[0].startswith('2'))
+    ):
         raise SystemError(
-            'Não foi possível localizar o artista da música solicitada.'
+            f'Error: {error_data[0]}\n Description: {error_data[1]}'
         )
 
+    next_page = title_song_content['response']['next_page']
+    page_number = next_page
+
+    return title_song_content, next_page
+
+
+def get_song_details(
+    music: str,
+    url_base: str,
+    artist_id: int,
+    header_api: dict[str, str],
+):
     page_number = 1
-    next_page = None
     validation_title = False
     title_song = ''
+    next_page = None
     while not next_page == 'null':
-        print('next_page:', page_number)
-        endpoint = f'artists/{artist_id}/songs?page={page_number}'
-        title_song_endpoint = ''.join((url_base, endpoint, '/songs'))
-        data2 = loads(
-            get(
-                url=title_song_endpoint,
-                headers=header,
-            ).content
+        title_song_content, page_number = get_song_content(
+            artist_id,
+            page_number,
+            url_base,
+            header_api,
         )
-        next_page = data2['response']['next_page']
 
-        if next_page is None:
-            next_page = 'null'
+        if page_number is None:
+            page_number = 'null'
             continue
 
-        page_number = next_page
+        (
+            validation_title,
+            title_song,
+            song_information
+        ) = get_song_information(
+            music,
+            title_song_content,
+        )
 
-        for response in data2['response']['songs']:
-            try:
-                infobox2 = {
-                    'title': response['title'],
-                    'url': ''
-                }
-
-                if (
-                    remover_acentos(infobox2['title'].upper())
-                    == remover_acentos(music).upper()
-                ):
-                    title_song = infobox2['title']
-                    infobox2['url'] = response['url']
-                    validation_title = True
-                    break
-
-            except:
-                ...
         if validation_title is True:
             break
 
-    if (next_page == 'null') and (title_song == ''):
-        raise SystemError(
-            'Não foi possível localizar a música solicitada.'
-        )
+    return next_page, title_song, song_information
 
-    cls()
 
-    html_content = get(infobox2['url'])
+def get_song_information(
+    music: str,
+    title_song_content: dict[str, str],
+):
+    for song_data in title_song_content['response']['songs']:
+        try:
+            song_information = {
+                'title': song_data['title'],
+                'url': ''
+            }
+
+            if (
+                remover_acentos(song_information['title'].upper())
+                == remover_acentos(music).upper()
+            ):
+                title_song = song_information['title']
+                song_information['url'] = song_data['url']
+                validation_title = True
+
+                break
+        except:
+            ...
+
+    return validation_title, title_song, song_information
+
+
+def extract_lyrics_content(
+    song_details: dict[str, str],
+) -> list[bytes]:
+    html_content = get(song_details['url'])
 
     validation_show = False
     validation_number = 0
@@ -134,11 +240,6 @@ def search_lyrics(artist, music,):
         b'n<a href=',
     ]
 
-    infobox3 = {
-        'artist_name': infobox1['artist_name'],
-        'title': infobox2['title']
-    }
-
     lyrics_content = []
     for item in b''.join(html_content.content.splitlines()).split(b'\\'):
         if item.__contains__(b'"body') and validation_number < 1:
@@ -151,8 +252,59 @@ def search_lyrics(artist, music,):
         if validation_show is True and item not in skiping_list:
             lyrics_content.append(item)
             ...
+    return lyrics_content
 
-    print(infobox3['artist_name'])
-    print(infobox3['title'], '\n')
+
+def search_lyrics(artist: str, music: str,):
+    header_api = get_api_auth_bearer()
+
+    url_base, base_data = get_artist_base_data(artist, header_api)
+
+    artist_data_information = get_artist_data_information(
+        artist,
+        base_data
+    )
+
+    artist_id = artist_data_information['artist_id']
+    if artist_id == '':
+        raise SystemError(
+            'Não foi possível localizar o artista da música solicitada.'
+        )
+
+    next_page, title_song, song_details = get_song_details(
+        music,
+        url_base,
+        artist_id,
+        header_api,
+    )
+
+    if (next_page == 'null') and (title_song == ''):
+        raise SystemError(
+            'Não foi possível localizar a música solicitada.'
+        )
+
+    cls()
+
+    lyrics_content = extract_lyrics_content(song_details)
+
+    show_lyrics_details(
+        artist_data_information,
+        song_details,
+        lyrics_content
+    )
+
+
+def show_lyrics_details(
+    artist_data_information: dict[str, str],
+    song_details: dict[str, str],
+    lyrics_content: list[bytes],
+):
+    song_metadata = {
+        'artist_name': artist_data_information['artist_name'],
+        'title': song_details['title']
+    }
+
+    print(song_metadata['artist_name'])
+    print(song_metadata['title'], '\n')
 
     [print(verse.decode(encoding='utf8')) for verse in lyrics_content]
--- lyrics_app.py
+++ lyrics_app.py
@@ -1,5 +1,8 @@
 from app.generic import search_lyrics
+from py_rpautom.python_utils import cls
 
+
+cls()
 
 artist = 'James Blunt'
 music = '1973'
